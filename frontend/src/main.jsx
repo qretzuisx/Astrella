430b629db6a421338823c561b872d3d3f4d92762
Rhizalyn <rhizalynespiritu@gmail.com>
2025-11-22 21:16:44 +0800

    chore(frontend): remove ErrorBoundary wrapper from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ErrorBoundary from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </BrowserRouter>,
 )
