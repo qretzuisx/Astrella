86aeafa6523895d7ef6518aa01b49379203d5c85
Rhizalyn <rhizalynespiritu@gmail.com>
2025-11-24 09:41:19 +0800

    Remove Featured Gowns section temporarily

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import FeaturedSection from '../components/FeaturedSection'
+// import FeaturedSection from '../components/FeaturedSection'
 
 const Home = () => {
   return (
     <div>
         <>
             <Hero/>
-            <FeaturedSection/>
+            {/* <FeaturedSection/> */}
         </>
     </div>
   )
